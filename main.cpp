16f2bdf5589316d1a8d2403d42698544850c6b75
Hassan Raza <razah1532@gmail.com>
2024-10-14 21:45:26 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   int a ;
+    float b;
+
+    cout << "By using comparison operators." << endl;
+    cout <<"Enter an Integer = "<<endl;
+    cin>>a;
+    cout << "Enter another Float = "<<endl;
+    cin>>b;
+
+    cout << "By using comparison operators." << endl;
+
+    cout<<"(a==b) = " << (a==b)<<endl;
+    cout<<"(a!=b) = " << (a!=b)<<endl;
+    cout<<"(a>b) = " << (a>b)<<endl;
+    cout<<"(a<b) = " << (a<b)<<endl;
+    cout<<"(a>=b) = " << (a>=b)<<endl;
+    cout<<"(a<=b) = " << (a<=b)<<endl;
+
+    return 0;
+
+}
+
